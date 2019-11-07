9b64ea7fcad228507ce4f94e6b801ba948080900
Sigit Prayitno <cybergitt@gmail.com>
2019-11-07 14:09:17 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Products from './Components/Products';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: '',
+      sort: '',
+      cartItems: [],
+      products: [],
+      filteredProducts: []
+    };
+  }
+
+  componentWillMount(){
+    if (localStorage.getItem('cartItems')) {
+      this.setState({ cartItems: JSON.parse(localStorage.getItem('cartItems')) });
+    }
+
+    fetch('http://localhost:8027/products/', {
+      headers: {
+        contentType: 'application/json'
+      }
+    }).then(res => res.json())
+      .then(data => {
+        this.setState({ products: data });
+        this.listProducts();
+      });
+  }
+
+  listProducts = () => {
+    this.setState(state => {
+      // Filtering and sorting go here.     
+      return { filteredProducts: state.products };
+    })
+  }
+
+  handleAddToCart = (e, product) => {
+    console.log("Add to cart");
+  }
+
+  render() {
+    return (
+      <div className="container">
+          <h1>E-Commerce Shopping Cart App</h1>
+          <hr />
+          <div className="row">
+            <div className="col-md-9">
+              <hr />
+              <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+            </div>
+            <div className="col-md-3">
+
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
